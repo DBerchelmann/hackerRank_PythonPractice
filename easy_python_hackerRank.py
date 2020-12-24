d7e749df33f14d93ce61f7d7a915c319f8e80ce9
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-23 23:20:04 -0600

    updated python

--- easy_python_hackerRank.py
+++ easy_python_hackerRank.py
@@ -32,4 +32,13 @@ if (n in range(6,21) or n % 2 != 0):
 else:
     print('Not Weird')
 
+# The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+# This one is really straightforward but you can get this all on one line and use the sep = '\n' to create seperation
+
+print((a + b), (a - b), (a * b), sep='\n')
 
